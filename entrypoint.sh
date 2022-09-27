b4ea76d01fe103488875964a21a5cbccd937b0f4
Ralph Cowling <ralph@ralph-cowling.com>
2022-09-27 15:31:24 +0200

    WIP just use blc args

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,57 +2,78 @@
 # Fail when any task exits with a non-zero error
 set -e
 
-# Env vars work! Great!
-echo "we got input_fail?" $input_fail
-echo "we got inputs_url?" $inputs_url
-echo "we got input_force_follow?" $input_force_follow
-# TODO
-# set all vars as named env vars
-# pipe all output into a md/output file
-# pass output to next steps
-# allow passing of all args to blc, --verbose, --filter-level, --get, --user-agent etc
-
 NC='\033[0m' # No Color
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 PURPLE='\033[0;34m'
 
+# Env vars work! Great!
+echo "env:"$env
+
+# Setup temporary file for output
+BLC_TMP="/tmp/blc/out.md"
+GITHUB_WORKFLOW_URL="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}?check_suite_focus=true"
+
+# Create temp dir
+mkdir -p "$(dirname $BLC_TMP)"
+
 # Install the broken-link-checker module globally on the docker instance
 npm i -g broken-link-checker -s
 
+# TODO
+# set all vars as named env vars ✅
+# pipe all output into a md/output file
+# pass output to next steps
+# allow passing of all args to blc, --verbose, --filter-level, --get, --user-agent etc
+
+# Options
+#   --exclude               A keyword/glob to match links against. Can be used multiple times.
+#   --exclude-external, -e  Will not check external links.
+#   --exclude-internal, -i  Will not check internal links.
+#   --filter-level          The types of tags and attributes that are considered links.
+#                             0: clickable links
+#                             1: 0 + media, iframes, meta refreshes
+#                             2: 1 + stylesheets, scripts, forms
+#                             3: 2 + metadata
+#                             Default: 1
+#   --follow, -f            Force-follow robot exclusions.
+#   --get, -g               Change request method to GET.
+#   --help, -h, -?          Display this help text.
+#   --input                 URL to an HTML document.
+#   --host-requests         Concurrent requests limit per host.
+#   --ordered, -o           Maintain the order of links as they appear in their HTML document.
+#   --recursive, -r         Recursively scan ("crawl") the HTML document(s).
+#   --requests              Concurrent requests limit.
+#   --user-agent            The user agent to use for link checks.
+#   --verbose, -v           Display excluded links.
+#   --version, -V           Display the app version.
+
 echo -e "$PURPLE=== BROKEN LINK CHECKER ===$NC"
 echo -e "Running broken link checker on URL: $GREEN $inputs_url $NC"
-
-# Create exclude and settings strings based on configuration
-EXCLUDE=""
-# SET_FOLLOW=""
-SET_RECURSIVE=""
 
 if [ -z "$inputs_url" ] || [ "$inputs_url" == 'https://github.com/roc/link-checker' ]; then
     echo -e "$YELLOW Warning: Running test on default URL, please provide a URL in your action.yml.$NC"
 fi
 
-# Set arguments for blc
-# --follow,  -f Force-follow robot exclusions.
-FOLLOW=$input_force_follow
-
-# [ "$2" == false ] && SET_FOLLOW="--follow"
-
-[ "$4" == true ] && SET_RECURSIVE="-ro"
-
-for PATTERN in ${3//,/ }; do
-    EXCLUDE+="--exclude $PATTERN "
-done
-
-# Echo settings if any are set
-echo -e "Configuration: \n Honor robot exclusions: $GREEN$2$NC, \n Exclude URLs that match: $GREEN$3$NC, \n Resursive URLs: $GREEN$4$NC"
-
 # TODO: execute using eval e.g.
 # eval lychee ${FORMAT} --output ${LYCHEE_TMP} ${ARGS}
-echo "blc $inputs_url $EXCLUDE --follow=$FOLLOW $SET_RECURSIVE -v"
+echo "blc $inputs_url $inputs_blc_args"
 
-blc $inputs_url $EXCLUDE --follow=$FOLLOW $SET_RECURSIVE -v
+# Run broken link checker, save to markdown file, also show stdout & sterr while running
+blc $inputs_url $inputs_blc_args 2>&1 | tee $BLC_TMP
+
+cat "${BLC_TMP}" >"${GITHUB_STEP_SUMMARY}"
+# TODO: is this line necessary for echoing cat??
+echo
+
+echo 'got this far?'
+
+# Pass link-checker exit code to next step
+echo ::set-output name=exit_code::$exit_code
+
+echo "[Full Github Actions output](${GITHUB_WORKFLOW_URL})"
+# echo "[Full Github Actions output](${GITHUB_WORKFLOW_URL})" >>"${BLC_TMP}"
 
 # Create command and remove extra quotes
 # Put result in variable to be able to iterate on it later
