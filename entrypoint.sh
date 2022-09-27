5cd854c69020282a97b7e4b6bbf372b738ceb342
Ralph Cowling <ralph@ralph-cowling.com>
2022-09-27 15:56:48 +0200

    echo lots of output in the result step

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 set -e
 
 NC='\033[0m' # No Color
-RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 PURPLE='\033[0;34m'
@@ -22,57 +21,43 @@ mkdir -p "$(dirname $BLC_TMP)"
 npm i -g broken-link-checker -s
 
 # TODO
+# switch inputs from numbered to named args and pass through using ENV kind of like https://github.com/lycheeverse/lychee-action/blob/master/action.yml ✅
 # set all vars as named env vars ✅
-# pipe all output into a md/output file
+# pipe all output into a md/output file ✅
+# allow passing of all args to blc, --verbose, --filter-level, --get, --user-agent etc ✅
+# Full list of options: https://github.com/stevenvachon/broken-link-checker#options
 # pass output to next steps
-# allow passing of all args to blc, --verbose, --filter-level, --get, --user-agent etc
+# capture  exit code and choose whether to use it or not ✅
+# make and store output of report to be passed to issue filing next step
+# Pass link-checker exit code to next step
 
-# Options
-#   --exclude               A keyword/glob to match links against. Can be used multiple times.
-#   --exclude-external, -e  Will not check external links.
-#   --exclude-internal, -i  Will not check internal links.
-#   --filter-level          The types of tags and attributes that are considered links.
-#                             0: clickable links
-#                             1: 0 + media, iframes, meta refreshes
-#                             2: 1 + stylesheets, scripts, forms
-#                             3: 2 + metadata
-#                             Default: 1
-#   --follow, -f            Force-follow robot exclusions.
-#   --get, -g               Change request method to GET.
-#   --help, -h, -?          Display this help text.
-#   --input                 URL to an HTML document.
-#   --host-requests         Concurrent requests limit per host.
-#   --ordered, -o           Maintain the order of links as they appear in their HTML document.
-#   --recursive, -r         Recursively scan ("crawl") the HTML document(s).
-#   --requests              Concurrent requests limit.
-#   --user-agent            The user agent to use for link checks.
-#   --verbose, -v           Display excluded links.
-#   --version, -V           Display the app version.
-
-echo -e "$PURPLE=== BROKEN LINK CHECKER ===$NC"
-echo -e "Running broken link checker on URL: $GREEN $inputs_url $NC"
+echo -e "$PURPLE=== BROKEN LINK CHECKER ===$NC" >>$BLC_TMP
+echo -e "Running broken link checker on URL: $GREEN $inputs_url $NC" >>$BLC_TMP
 
 if [ -z "$inputs_url" ] || [ "$inputs_url" == 'https://github.com/roc/link-checker' ]; then
-    echo -e "$YELLOW Warning: Running test on default URL, please provide a URL in your action.yml.$NC"
+    echo -e "$YELLOW Warning: Running test on default URL, please provide a URL in your action.yml.$NC" >>$BLC_TMP
 fi
 
-# TODO: execute using eval e.g.
-# eval lychee ${FORMAT} --output ${LYCHEE_TMP} ${ARGS}
-echo "blc $inputs_url $inputs_blc_args"
-
 # Run broken link checker, save to markdown file, also show stdout & sterr while running
-blc $inputs_url $inputs_blc_args 2>&1 | tee $BLC_TMP
-
-cat "${BLC_TMP}" >"${GITHUB_STEP_SUMMARY}"
-# TODO: is this line necessary for echoing cat??
-echo
-
-echo 'got this far?'
+# Use eval to capture exit_code and use later
+echo "blc $inputs_url $inputs_blc_args"
+eval blc $inputs_url $inputs_blc_args 2>&1 | tee $BLC_TMP
+exit_code=$?
 
 # Pass link-checker exit code to next step
 echo ::set-output name=exit_code::$exit_code
 
-echo "[Full Github Actions output](${GITHUB_WORKFLOW_URL})"
+cat "${BLC_TMP}" >"${GITHUB_STEP_SUMMARY}"
+
+echo "[Full Github Actions output](${GITHUB_WORKFLOW_URL})" 2>&1 | tee $INPUT_OUTPUT
+echo ::set-output name=result::"$BLC_TMP"
+
+# If `inputs_allow_failures` is set to `true`, propagate the real exit value to the workflow
+# runner. This will cause the pipeline to fail on exit != 0.
+if [ "$inputs_allow_failures" = true ]; then
+    exit ${exit_code}
+fi
+
 # echo "[Full Github Actions output](${GITHUB_WORKFLOW_URL})" >>"${BLC_TMP}"
 
 # Create command and remove extra quotes
@@ -91,8 +76,6 @@ echo "[Full Github Actions output](${GITHUB_WORKFLOW_URL})"
 # else
 #     BROKEN_COUNT=0
 # fi
-
-# exit_code=$?
 
 # # Return results
 # if [ "$BROKEN_COUNT" -gt 0 ]; then
